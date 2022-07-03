27da34780aaf721d97587e648b939279a0bb13f1
Annu <annusinghraj1998@gmail.com>
2022-07-03 13:38:42 +0530

    ADDED: Repeat till player reaches 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,31 +9,29 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int position = 0;
 
-        Random ran = new Random();
-        int dice = ran.nextInt(6)+1;
-        System.out.println("dice: "+dice);
-        int optCheck = ran.nextInt(3);
-        System.out.println("optCheck: "+optCheck);
+        while (position < 100) {
+            Random random = new Random();
+            int dice = random.nextInt(6) + 1;
 
-        if (optCheck == ladder)
-        {
-            System.out.println("Ladder");
-            position = position + dice;
+            System.out.println("dice: " + dice);
+            int optCheck = random.nextInt(3);
+            System.out.println("optCheck: " + optCheck);
+
+            if ((optCheck == ladder) && (position + dice) <= 100) {
+                System.out.println("Ladder");
+                position = position + dice;
+            } else if (optCheck == snake) {
+                System.out.println("Snake");
+                position = position - dice;
+            } else {
+                System.out.println("No Play");
+            }
+            if (position < 0)
+                position = 0;
+            System.out.println("position: " + position);
         }
 
-        else if (optCheck == snake)
-        {
-            System.out.println("Snake");
-            position = position - dice;
-        }
-        else {
-            System.out.println("No Play");
-        }
-        if (position<0)
-            position=0;
-        System.out.println("position: "+position);
     }
-
 }
 
 
