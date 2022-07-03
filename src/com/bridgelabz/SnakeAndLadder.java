ab14073a5fa532a13ca088f1701c1e9f82b0faff
Annu <annusinghraj1998@gmail.com>
2022-07-03 13:22:53 +0530

    ADDED: Start snake and ladder game

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,5 +3,10 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program!");
+
+        int player1Position = 0;
+        System.out.println("Game begins: player1 at position: " + player1Position);
+
+
     }
 }
