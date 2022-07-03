5191237c9d078f43deef2d0c893ad18520312382
Annu <annusinghraj1998@gmail.com>
2022-07-03 13:47:29 +0530

    ADDED: Calculate number of times dice roll to win the game

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,42 +5,38 @@ public class SnakeAndLadder {
     static final int noPlay = 0;
     static final int ladder = 1;
     static final int snake = 2;
-
+    static int count = 0;
 
     public static void main(String[] args) {
         int position = 0;
 
         while (position < 100) {
             Random random = new Random();
-            int dice = random.nextInt(6)+1;
+            int dice = random.nextInt(6) + 1;
+            ++count;
 
             System.out.println("dice: " + dice);
             int optCheck = random.nextInt(3);
             System.out.println("optCheck: " + optCheck);
 
-            if ((optCheck == ladder) && (position + dice) <= 100)
-            {
+            if ((optCheck == ladder) && (position + dice) <= 100) {
                 System.out.println("Ladder");
                 position = position + dice;
-            }
-
-            else if (optCheck == snake)
-            {
+            } else if (optCheck == snake) {
                 System.out.println("Snake");
                 position = position - dice;
-            }
-
-            else
-            {
+            } else {
                 System.out.println("No Play");
             }
 
             if (position < 0)
                 position = 0;
 
-            System.out.println("position: " + position);
+            System.out.println("position: " + position + "\nDice count " + count);
         }
 
     }
 }
+
+
 
